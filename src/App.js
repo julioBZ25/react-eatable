4b0f03c5f98ef6e069e5abc87034d764e6a0946f
Julio Bazalar <jbzuloaga@gmail.com>
2022-08-29 22:12:46 -0500

    Update readme and details/features of the web app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/categories" element={<CategoriesDashboard/>} />
+        <Route path="/" element={<CategoriesDashboard/>} />
         <Route path="/categories/:category" element={<ProductsDashboard />} />
         <Route path="/create" element={<CreateProduct/>} />
         <Route path="/product/edit/:id" element={<EditProduct/>} />
